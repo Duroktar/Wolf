9fc037b68b749da6481d334292a2b989e7057c8b
Almenon <almenon214@gmail.com>
2018-03-01 21:22:20 -0800

    first test #5

---
+++ scripts/unitTests.py
@@ -0,0 +1,16 @@
+import unittest
+import wolf
+import os
+
+
+class TestPythonEvaluator(unittest.TestCase):
+
+    testDir = os.path.dirname(__file__) + '/testInputs/'
+
+    def test_simple_code(self):
+        returnStatus = wolf.main(self.testDir + "simplestTest.py")
+        assert returnStatus != 1
+
+
+if __name__ == '__main__':
+    unittest.main()
