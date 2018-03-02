c2cd5f7ffd2acbf0d64923cbc92036e196399416
Almenon <Almenon@users.noreply.github.com>
2018-03-01 22:02:09 -0800

    #5 another test case

--- scripts/unitTests.py
+++ scripts/unitTests.py
@@ -7,6 +7,10 @@ class TestPythonEvaluator(unittest.TestCase):
 
     testDir = os.path.dirname(__file__) + '/testInputs/'
 
+    def test_simple_output(self):
+        returnStatus = wolf.main(self.testDir + "simpleOutput.py")
+        assert returnStatus != 1
+
     def test_simple_code(self):
         returnStatus = wolf.main(self.testDir + "simplestTest.py")
         assert returnStatus != 1
