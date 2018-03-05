49818ac950488df655f6aa4ea842261f038baff2
traBpUkciP <scott.doucet@hotmail.com>
2018-03-05 03:37:06 -0400

    Added a snapshot test for the tracer file to our unittests
    - The snapshot tests for most features. SO now that we have a good
    working base we can be pretty confident it's not broken if it passes.
    
    - Cleaned up some other tests ..
    
    - Redirected stdout during testing using context managers. Comment
    em out if you really need to see the output.
    
    TO TEST:
    
    $ pip install nip.cli
    $ nip install
    $ nip test:unittests
    
    Warning: I wrote nip too, lol

--- scripts/unitTests.py
+++ scripts/unitTests.py
@@ -1,28 +1,63 @@
 import unittest
-import wolf
 import os
-
-
-def dirFactory(base):
-    return lambda target: os.path.join(base, target)
+import io
+import wolf
+from contextlib import redirect_stdout, contextmanager
 
 
 __dirname = os.path.dirname(__file__)
 testDir = os.path.join(__dirname, 'testInputs')
-getTarget = dirFactory(testDir)
+
+
+@contextmanager
+def buffer_stream():
+    buffer = io.StringIO()
+    yield buffer
+    buffer.close()
+
+
+def dir_factory(base):
+    return lambda target: os.path.join(base, target)
+
+
+get_target = dir_factory(testDir)
+
+
+def get_snapshot(target):
+    with open(get_target(target)) as fs:
+        return fs.read()
 
 
 class TestPythonEvaluator(unittest.TestCase):
 
+    def setUp(self):
+        wolf.WOLF = []
+
     def test_simple_output(self):
-        target = getTarget("simpleOutput.py")
-        returnStatus = wolf.main(target)
-        assert returnStatus != 1
+        target = get_target("simpleOutput.py")
+        with buffer_stream() as buf:
+            with redirect_stdout(buf):
+                returnStatus = wolf.main(target)
+                assert returnStatus != 1
 
     def test_simple_code(self):
-        target = getTarget("simplestTest.py")
-        returnStatus = wolf.main(target)
-        assert returnStatus != 1
+        # Returns 1 if no annotations to print
+        target = get_target("simplestTest.py")
+        with buffer_stream() as buf:
+            with redirect_stdout(buf):
+                returnStatus = wolf.main(target)
+                assert returnStatus == 1
+
+    def test_snapshot(self):
+        target = get_target("complex.test.py")
+        snapshot = get_snapshot("complex.snapshot").strip()
+        with buffer_stream() as buf:
+            with redirect_stdout(buf):
+                returnStatus = wolf.main(target)
+                assert returnStatus != 1
+            result = buf.getvalue().strip()
+        self.maxDiff = None
+        self.assertMultiLineEqual(result, snapshot)
 
 
 if __name__ == '__main__':
